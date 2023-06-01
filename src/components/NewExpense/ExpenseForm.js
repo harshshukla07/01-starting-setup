22a6413fe2620905a902f2127d69eddad8556a63
Harsh Shukla <harshshukla5810@gmail.com>
2023-06-02 00:15:26 +0530

    Adding NewExpense Component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,6 +17,15 @@ const ExpenseForm = () =>
                     <input type="number" min="0.01" step="0.01" />
                 </div>
             </div>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label htmlFor="">Date</label>
+                    <input type="date" min="2023-06-01" max="2050-12-31" />
+                </div>
+            </div>
+            <div className='new-expense__actions'>
+                <button type='submit'>Add Expense</button>
+            </div>
         </form>
     );
 };
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () =>
 {
     return (
         <div className='new-expense'>
-            <form action=""></form>
+            <ExpenseForm />
         </div>
     );
 };
